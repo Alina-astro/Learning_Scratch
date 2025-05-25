b024d28dab485b5659fca879717d29d132981f5d
Alina Nadtochiy <am0817@mail.ru>
2025-05-26 02:23:45 +0300

    add link main page on logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './Header.module.scss';
 import logo from '../../assets/icons/logo_header.png';
 
 export default function Header({ onLogin, onRegister }) {
   return (
     <header className={styles.header}>
-      <img src={logo} alt="Лого Код Изумрудного города" className={styles.logo} />
+      <Link to="/">
+    <img
+      src={logo}
+      alt="Лого Код Изумрудного города"
+      className={styles.logo}
+    />
+  </Link>
       <div className={styles.buttons}>
         <button onClick={onRegister}>Регистрация</button>
         <button onClick={onLogin}>Вход</button>
